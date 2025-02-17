9e23cb04da140f46dfedbbff9d34fffa78e82dda
tomasndlate <tomasndlate@gmail.com>
2025-02-17 16:22:29 +0000

    secrets conflict

--- src/app/(private)/dashboard/credits/credits-card.tsx
+++ src/app/(private)/dashboard/credits/credits-card.tsx
@@ -2,7 +2,7 @@
 import { loadStripe } from '@stripe/stripe-js';
 import { buyCredits } from './actions';
 
-const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY!);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 export default function CreditsCard({ price }: { price: 50 | 100 | 250 }) {
 
