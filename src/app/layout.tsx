3c67e8205f50f991b42200d74b0ce0501f833866
tomasndlate <tomasndlate@gmail.com>
2025-02-21 16:48:43 +0000

    icon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import "@/styles/globals.css";
 export const metadata: Metadata = {
   title: "MLS Models",
   description: "Machine Learning Subscription models to make predictions.",
+  icons:{
+    icon:"/images/icon.svg"
+  }
 };
 
 export default function RootLayout({
